c60e42d26faf0065ecbe939798b119c46b8951a3
Bk10ST <133320594+Bk10ST@users.noreply.github.com>
2024-04-03 03:38:57 -0700

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SupplierDashboard from './Dashboard/SupplierDashboard'
+
+const App = () => {
+  return (
+    <div>
+ <SupplierDashboard/>
+    </div>
+  )
+}
+
+export default App
